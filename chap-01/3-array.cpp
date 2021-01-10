18f5c437864b0f4523eb0e8439046eea6637f936
mportefaix <maxime.portefaix@gmail.com>
2021-01-10 16:51:26 +0100

    array behaviour tested

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
+#include <vector>
 
-int main()
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    int array[] = { 0, 1, 2, 3 };
-
-    for (int i = 0; i < 4; ++i)
+    if (argc != 2)
     {
-        std::cout << array[i] << std::endl;
+        cerr << "Need one argument, " << argc - 1 << " where given" << endl;
+        return -1;
+    }
+    
+    /*
+    auto size = 50;
+    int values[size] = {};
+    for (int i = 0; i < size; ++i)
+    {
+        values[i] = i + 1;
+    }
+
+    for (auto value : values)
+    {
+        std::cout << value << std::endl;
+    }
+    */
+
+    cout << "Size du tableau : " << argv[1] << endl;
+    int length = stoi(argv[1]);
+    
+    /*
+    int* array = new int[length] {};
+    for (auto i = 0; i < length; ++i) {
+        cout << array[i] << endl;
+    }
+    */
+
+    vector<int> array;
+    for(auto i = 0; i < length; ++i)
+    {
+        array.emplace_back(i + 1);
+    }
+    
+    for(auto value : array)
+    {
+        cout << value << endl;
     }
 
     return 0;
