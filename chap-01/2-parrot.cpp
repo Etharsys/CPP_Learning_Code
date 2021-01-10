354b27f701622018aed9f43408d5aca194d82f70
mportefaix <maxime.portefaix@gmail.com>
2021-01-10 16:18:49 +0100

    parrot behaviours tested

--- chap-01/2-parrot.cpp
+++ chap-01/2-parrot.cpp
@@ -1,1 +1,47 @@
-// Implement the program here.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /*
+    int number = 0;
+    */
+
+    /*
+    while (true)
+    {
+        cin >> number;
+        cout << number << endl;
+        if (number == -1)
+        {
+            break;
+        }
+    }
+    */
+
+    /*
+    do
+    {
+        cin >> number;
+        cout << number << endl;
+    } while (number != -1);
+    */
+
+    /*
+    for (auto i = 0; i < 10; ++i){
+        cin >> number;
+        cout << number << endl;
+    }
+    */
+
+    string word;
+    for (auto i = 0; i < 10; ++i){
+        cin >> word;
+        cout << "Croaw " << word << "!" << endl;
+    }
+    
+
+    return 0;
+}
