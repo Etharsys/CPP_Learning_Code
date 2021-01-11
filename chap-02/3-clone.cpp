fc90c2e4b3410c9f887b3812e9c23179eec87728
mportefaix <maxime.portefaix@gmail.com>
2021-01-11 15:24:56 +0100

    clone methods tested

--- chap-02/3-clone.cpp
+++ chap-02/3-clone.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class Person
 {
 public:
     Person(const std::string& name, const std::string& surname)
         : _name { name }, _surname { surname }
     {}
+    Person(const Person& original)
+        : _name { original._name }, _surname { original._surname }
+    {}
+
+    Person& operator=(const Person& original)
+    {
+        if (this != &original)
+        {
+            _name = original._name;
+            _surname = original._surname;
+        }
+        return *this;
+    } 
 
     std::string  get_full_name() const { return _name + " " + _surname; }
     unsigned int get_age() const  { return _age; }
@@ -19,12 +34,26 @@ private:
     unsigned int _age = 0u;
 };
 
+ostream& operator<< (ostream& stream, const Person& person)
+{
+    return stream << "Person named '" << person.get_full_name() << "' is " << person.get_age() << " years old.";
+}
+
 int main()
 {
     Person batman { "Bruce", "Wayne" };
     batman.wait(23);
 
-    std::cout << "Person named '" << batman.get_full_name() << "' is " << batman.get_age() << " years old." << std::endl;
+    std::cout << batman << std::endl;
+
+    auto batman_le_retour = batman;
+    std::cout << batman_le_retour << std::endl;
+
+    Person assigned_clone { "Batman", "2" };
+    cout << assigned_clone << endl;
+
+    assigned_clone = batman;
+    cout << assigned_clone << endl;
 
     return 0;
 }
