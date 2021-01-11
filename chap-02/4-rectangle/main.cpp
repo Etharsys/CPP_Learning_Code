5d9b23e2ef7ee2b79ec9d8e8d5ccf1bc28921d8f
mportefaix <maxime.portefaix@gmail.com>
2021-01-11 16:28:54 +0100

    Rectangle classe complete

--- chap-02/4-rectangle/main.cpp
+++ chap-02/4-rectangle/main.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
     Rectangle rect(2.f, 4.f);
@@ -9,6 +11,26 @@ int main()
 
     rect.scale(3);
     std::cout << "{ L: " << rect.get_length() << ", W: " << rect.get_width() << " }" << std::endl;
+
+    Rectangle square(2.5);
+    std::cout << "{ L: " << square.get_length() << ", W: " << square.get_width() << " }" << std::endl;
+
+    Rectangle::set_default_size(3.f);
+    Rectangle s1;
+    Rectangle s2;
+
+    Rectangle::set_default_size(5.f);
+    Rectangle s3;
+    Rectangle s4;
+    Rectangle s5;
+
+    // At this point, size of s1 and s2 should be 3.f, and size fo s3, s4 and s5 should be 5.f. 
+
+    cout << s1 << endl;
+    cout << s2 << endl;
+    cout << s3 << endl;
+    cout << s4 << endl;
+    cout << s5 << endl;
 
     return 0;
 }
